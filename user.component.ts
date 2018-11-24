d1ecbc9a7e3eee964dbb19ccb1640ae643ffb340
Harshkaur <harshgeetkaur@yahoo.com>
2018-11-24 16:15:54 +0530

    First commit

---
+++ user.component.ts
@@ -0,0 +1,270 @@
+﻿import { Component, ViewEncapsulation, ViewChild, TemplateRef, OnInit, AfterViewInit, AfterContentChecked } from '@angular/core';
+import { Validators, FormGroup, FormArray, FormBuilder, FormControl, AbstractControl, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { NgbModal, NgbDateStruct, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'angular-2-dropdown-multiselect';
+import { UserService } from "../user/user.service";
+import { UserViewModel, StateViewModel, PermissionViewModel, CityViewModel } from './user.model';
+import {
+    DataDocument, MetaTypeSource, MetaType, DataDocumentSource, ObjectDataNode,
+    IPropertyDataNode, TextPropertyDataNode, PropertyDataNodeKeyedBag, ObjectGenericDataNode
+} from "../index";
+import { XmlMap } from "../bridge";
+import { AppService } from "../app.service";
+import { AppCommon } from "../app.common";
+import * as d from "../data";
+import { AppConstant } from '../app.constant';
+import { switchMap, debounceTime, tap, finalize } from 'rxjs/operators';
+declare var $: any;
+@Component({
+    selector: 'app-user-detail',
+    templateUrl: './user.component.html',
+    encapsulation: ViewEncapsulation.None,
+    providers: [UserService, AppService],
+    styleUrls: ['./user.component.css']
+})
+
+export class UserComponent implements OnInit, AfterViewInit, AfterContentChecked {
+
+    public appCommon: AppCommon;
+    public userForm: FormGroup; // our form model
+    static Doc: DataDocument;
+    public userId: string = "";
+    public doc: DataDocument;
+    public cityList: CityViewModel[] = [];
+    public permissionList: IMultiSelectOption[] = [];
+    constructor(private _AppService: AppService,
+        private _UserService: UserService,
+        private router: Router,
+        private location: Location,
+        private _fb: FormBuilder,
+        private route: ActivatedRoute) {
+        this.appCommon = new AppCommon(router);
+        this.addhtmltoform(new UserViewModel());
+        this.permissionList = this.appCommon.GetPermissionList();
+    }
+
+    ngAfterViewInit() {
+
+    }
+    ngAfterContentChecked() {
+
+    }
+    ngOnInit() {
+
+        //this.cityList = this.appCommon.GetCityList();
+        this.route.params.subscribe(params => {
+            this.userId = params[AppConstant.ParameterId] as string; //log the value of id
+            if (this.userId == null || this.userId == undefined)
+                this.userId = "";
+        });
+        if (this.userId == "") {
+            var type = MetaTypeSource.Instance.GetMetaType(AppConstant.XMLPathUser);
+            this.doc = new DataDocument({ type: type });
+            this.doc.RootObject = new ObjectDataNode({ type: type });
+
+        }
+        else {
+            var docXml = "";
+            this._UserService.getUser(this.userId).subscribe(res => {
+                docXml = res;
+                this.doc = DataDocument.FromXmlNode(XmlMap.parse(docXml));
+                this.doc.Uri = AppConstant.FolderUser + this.userId + AppConstant.XmlExtension;
+                var editModel: UserViewModel = new UserViewModel();
+                var propertyNodes: PropertyDataNodeKeyedBag = this.doc.RootObject.PropertyNodes;
+                editModel.FirstName = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.FirstName);
+                editModel.Email = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.Email);
+                editModel.LastName = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.LastName);
+                editModel.Title = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.Title);
+                editModel.Institution = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.Institution);
+                editModel.CellPhone = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.CellPhone);
+                editModel.OfficePhone = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.OfficePhone);
+                editModel.Street = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.Street);
+                //editModel.City = this.appCommon.GetLookupSingleNodeValue(propertyNodes, AppConstant.City);
+                //editModel.State = this.appCommon.GetLookupSingleNodeValue(propertyNodes, AppConstant.State);
+                editModel.Zip = this.appCommon.GetTextPropertyNodeValue(propertyNodes, AppConstant.Zip);
+                editModel.Permission = this.appCommon.GetLookupMultipleNodeValue(propertyNodes, AppConstant.Permission).map(x => x.Uri);
+
+                var cityURL: string = this.appCommon.GetLookupSingleNodeValue(propertyNodes, AppConstant.City);
+                var stateURL: string = this.appCommon.GetLookupSingleNodeValue(propertyNodes, AppConstant.State);
+                this._UserService.read(cityURL).subscribe((cityResponse: string) => {
+                    var cityDoc = DataDocument.FromXmlNode(XmlMap.parse(cityResponse));
+                    editModel.City = new CityViewModel();
+                    editModel.City.Name = this.appCommon.GetDisplayText(cityDoc);
+                    editModel.City.Uri = cityURL;
+
+
+                    this._UserService.read(stateURL).subscribe((stateResponse: string) => {
+                        var stateDoc = DataDocument.FromXmlNode(XmlMap.parse(stateResponse));
+                        editModel.State = new StateViewModel();
+                        editModel.State.Name = this.appCommon.GetDisplayText(stateDoc);
+                        editModel.State.Uri = stateURL;
+                        editModel.State.Abbrev = this.appCommon.GetValueText(stateDoc);
+                        this.addhtmltoform(editModel);
+                        this.getCityMatchText(editModel.City.Name);
+                        this.getStateMatchText(editModel.State.Abbrev);
+
+                    });
+
+
+
+                });
+
+
+            });
+
+        }
+
+
+    }
+
+    public addhtmltoform(model: UserViewModel) {
+        this.userForm = this._fb.group({
+            FirstName: [model.FirstName, [Validators.required, Validators.maxLength(500)]],
+            LastName: new FormControl(model.LastName, [Validators.required, Validators.maxLength(500)]),
+            Email: new FormControl(model.Email, [Validators.required, Validators.email, Validators.maxLength(500)]),
+            Title: [model.Title, [Validators.required, Validators.maxLength(500)]],
+            Institution: [model.Institution, [Validators.required, Validators.maxLength(500)]],
+            CellPhone: [model.CellPhone, [Validators.required, Validators.maxLength(15)]],
+            OfficePhone: [model.OfficePhone, [Validators.required, Validators.maxLength(15)]],
+            Street: [model.Street, [Validators.required, Validators.maxLength(500)]],
+            City: [model.City, [Validators.required]],
+            State: [model.State, [Validators.required]],
+            Zip: [model.Zip, [Validators.required, Validators.maxLength(10)]],
+            Permission: [model.Permission],
+        });
+    }
+
+    public SaveUser(user: UserViewModel) {
+        if (user.City.Uri === "" || user.City.Uri === null || user.City.Uri === undefined) {
+            alert("Please select city.");
+            return;
+        }
+        if (user.State.Uri === "" || user.State.Uri === null || user.State.Uri === undefined) {
+            alert("Please select state.");
+            return;
+        }
+        var propertyNodes: PropertyDataNodeKeyedBag = this.doc.RootObject.PropertyNodes;
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.FirstName, user.FirstName);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.LastName, user.LastName);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.Email, user.Email.toLowerCase());
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.Title, user.Title);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.Institution, user.Institution);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.CellPhone, user.CellPhone);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.OfficePhone, user.OfficePhone);
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.Street, user.Street);
+        this.appCommon.SetLookupSingleNodeValue(propertyNodes, AppConstant.City, this.appCommon.GetSelectedDataDocumentFromLookUpSingle(this.result, user.City.Uri));
+        this.appCommon.SetLookupSingleNodeValue(propertyNodes, AppConstant.State, this.appCommon.GetSelectedDataDocumentFromLookUpSingle(this.stateResult, user.State.Uri));
+        this.appCommon.SetTextPropertyNodeValue(propertyNodes, AppConstant.Zip, user.Zip);
+        this.appCommon.ClearLookupMultipleNodeValue(propertyNodes, AppConstant.Permission);
+        for (var per of user.Permission) {
+            this.appCommon.SetLookupMultipleNodeValue(propertyNodes, AppConstant.Permission, this.appCommon.GetSelectedDataDocumentFromLookUpSingle(this.appCommon.GetPermissionDocumentList(), per));
+        }
+        this.doc.Uri = AppConstant.FolderUser + this.doc.RootObject.SId + AppConstant.XmlExtension;
+        this._AppService.saveXML(this.doc).subscribe(() => {
+            this.router.navigate([AppConstant.UserListURL]);
+        });
+
+    }
+    public redirectToListPage() {
+        this.router.navigate([AppConstant.UserListURL]);
+    }
+
+    public result: DataDocument[] = [];
+    public getCityMatchText(data: string) {
+        if (data == null || data == undefined || data == "")
+            data = $('#cityTextBox').val();
+        this.cityList = [];
+        if (data != null && data != undefined && data.length > 2) {
+            this.isLoading = true;
+            this._UserService.readByText("/Core/types/City.xml", "Name", data).subscribe((responsestr: string) => {
+                this.result = [];
+                var xroot = XmlMap.parse(responsestr);
+                XmlMap.eachChild(xroot, (child, i) => {
+                    var uri = XmlMap.getChildTextByName(child, "Uri");
+                    var docNode = XmlMap.getChildByName(child, "Doc").children[0];
+                    var doc = DataDocument.FromXmlNode(docNode);
+                    doc.Uri = uri;
+                    this.result.push(doc)
+                });
+                for (let cityDocument of this.result) {
+                    var city = new CityViewModel();
+                    city.Name = this.appCommon.GetDisplayText(cityDocument);
+                    city.Uri = cityDocument.Uri;
+                    this.cityList.push(city);
+                }
+                this.isLoading = false;
+            });
+        }
+    }
+    public isLoading = false;
+
+    displayFn(city: CityViewModel) {
+        if (city) { return city.Name; }
+    }
+    public cityClosed() {
+        var cityValue = this.userForm.controls["City"].value;
+        if (cityValue.Name == undefined) {
+            this.userForm.controls["City"].setValue("");
+            $('#cityTextBox').val("");
+            this.cityList = [];
+            this.result = [];
+        }
+    }
+
+    public cityOpened() {
+    }
+
+    //======= StateViewModel 
+    public stateList: StateViewModel[] = [];
+    public stateResult: DataDocument[] = [];
+    public isStateLoading: boolean = false;
+    public getStateMatchText(data: string) {
+        if (data == null || data == undefined || data == "")
+            data = $('#stateTextBox').val();
+        this.stateList = [];
+        if (data != null && data != undefined && data.length > 0) {
+            this.isStateLoading = true;
+            this._UserService.readByText("/Core/types/State.xml", "Abbrev,Name", data.split(' -')[0]).subscribe((responsestr: string) => {
+                this.stateResult = [];
+                var xroot = XmlMap.parse(responsestr);
+                XmlMap.eachChild(xroot, (child, i) => {
+                    var uri = XmlMap.getChildTextByName(child, "Uri");
+                    var docNode = XmlMap.getChildByName(child, "Doc").children[0];
+                    var doc = DataDocument.FromXmlNode(docNode);
+                    doc.Uri = uri;
+                    this.stateResult.push(doc);
+                });
+                for (let stateDocument of this.stateResult) {
+                    var state = new StateViewModel();
+                    state.Name = this.appCommon.GetDisplayText(stateDocument);
+                    state.Abbrev = this.appCommon.GetValueText(stateDocument);
+                    state.Uri = stateDocument.Uri;
+                    this.stateList.push(state);
+                }
+                this.isStateLoading = false;
+
+            });
+        }
+    }
+    displayFnState(state: StateViewModel) {
+        if (state && state.Abbrev != undefined) { return state.Abbrev + ' - ' + state.Name; }
+    }
+    public stateClosed() {
+        var stateValue = this.userForm.controls["State"].value;
+        if (stateValue.Name == undefined) {
+            this.userForm.controls["State"].setValue("");
+            $('#stateTextBox').val("");
+            this.stateList = [];
+            this.stateResult = [];
+        }
+    }
+
+    public stateOpened() {
+    }
+}
+
---
+++ user.service.ts
@@ -0,0 +1,94 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
+let apiURL = environment.apiEndpoint;
+import { AppConstant } from '../app.constant';
+import {
+    DataDocument, MetaTypeSource, MetaType, DataDocumentSource, ObjectDataNode,
+    IPropertyDataNode, TextPropertyDataNode, PropertyDataNodeKeyedBag, ObjectGenericDataNode
+} from "../index";
+import { UserViewModel, UserFilterModel } from './user.model';
+import { AuthenticatedHttpService } from '../../app/authentication/customauthentication.http.service';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { ReflectiveInjector } from '@angular/core';
+import { map } from 'rxjs/operators';
+@Injectable()
+export class UserService {
+
+    constructor(public http: AuthenticatedHttpService) {
+    }
+
+    getUser(userId: string): Observable<any> {
+        return this.http.get(apiURL + '/user/Read?uri=/user/' + userId + '.xml').map(response => response.text());
+    }
+    deleteUser(userId: string): Observable<any> {
+        return this.http.get(apiURL + '/user/DeleteUser?uri=/user/' + userId + '.xml').map(response => response.text());
+    }
+    getRegistrationV1(registrationId: string): Observable<any> {
+        return this.http.get(apiURL + '/login/GetRegistration?registrationId=' + registrationId).map(response => response.text());
+    }
+    changeUserStatus(userId: string): Observable<any> {
+        return this.http.get(apiURL + '/user/ChangeUserStatus?UserUri=/user/' + userId + '.xml').map(response => response.text());
+    }
+    deleteRegistration(regId: string): Observable<any> {
+        return this.http.get(apiURL + '/user/Delete?uri=/user/invite/' + regId + '.xml').map(response => response.text());
+    }
+    reInviteUser(uri: string): Observable<any> {
+        return this.http.get(apiURL + '/user/ReInviteUser?uri=' + uri).map(response => response.text());
+    }
+    logoutUser(): Observable<any> {
+        return this.http.get(apiURL + '/login/logout').map(response => response.json());
+    }
+    public saveUser(doc: DataDocument, registrationId: string, authDoc: DataDocument) {
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+        var xml = doc.ToXml();
+        xml = xml.replace(/<><\/>/g, "");
+        var authxml = authDoc.ToXml();
+        authxml = authxml.replace(/<><\/>/g, "");
+        var data = new URLSearchParams();
+        data.set('xml', encodeURI(xml));
+        data.set('uri', doc.Uri);
+        data.set('name', registrationId);
+        data.set('childXml', encodeURI(authxml));
+        data.set('childUri', authDoc.Uri);
+        data.set('childName', '');
+        return this.http.post(apiURL + '/login/SaveUser', data.toString(), options).map(response => response.json());
+    }
+    sendForgotPasswordEmailV1(email: string): Observable<any> {
+        return this.http.get(apiURL + '/login/SendForgotPasswordEmailV1?email=' + email).map(response => response.json());
+    }
+    getResetPasswordV1(resetPasswordId: string): Observable<any> {
+        return this.http.get(apiURL + '/login/GetResetPassword?uri=/user/resetpassword/' + resetPasswordId + '.xml').map(response => response.text());
+    }
+    public saveResetPassword(hashPassword: string, registrationId: string, dataURI: string) {
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers });
+        var data = new URLSearchParams();
+        data.set('xml', hashPassword);
+        data.set('uri', dataURI);
+        data.set('name', registrationId);
+        //data.set('loggedInUserURI', sessionStorage["UserId"]);
+        return this.http.post(apiURL + '/login/SaveResetPassword', data.toString(), options).map(response => response.text());
+    }
+    public getUserList(model: UserFilterModel) {
+        let headers = new Headers({ 'Content-Type': "application/json" });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(apiURL + '/user/GetUserList', model, options).map(response => response.json());
+    }
+    public getUserInviteList(model: UserFilterModel) {
+        let headers = new Headers({ 'Content-Type': "application/json" });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(apiURL + '/user/GetUserInviteList', model, options).map(response => response.json());
+    }
+    public readByText(typeURI: string, propertyName: string, searchText: string) {
+        return this.http.get(apiURL + "/login/MatchText?typeURI=" + typeURI + "&propertyName=" + propertyName + "&searchText=" + searchText + "&z=" + (new Date())).map(response => response.json());
+    }
+    read(uri: string): Observable<any> {
+        return this.http.get(apiURL + '/user/Read?uri=' + uri).map(response => response.text());
+    }
+    
+}
